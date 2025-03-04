1d2ebf0d3d14fd8a443cd78bf473c2840a8db5bd
Renzo Uribe <renzouribe2010@gmail.com>
2025-03-04 13:34:04 -0500

    rename to localai

--- block_localai_chat.php
+++ block_localai_chat.php
@@ -17,14 +17,15 @@
 /**
  * Block class
  *
- * @package    block_openai_chat
+ * @package    block_localai_chat
  * @copyright  2023 Bryce Yoder <me@bryceyoder.com>
+ * @copyright  2025 Renzo Uribe <renzouribe2010@gmail.com> (modifications: rename to localai_chat)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-class block_openai_chat extends block_base {
+class block_localai_chat extends block_base {
     public function init() {
-        $this->title = get_string('openai_chat', 'block_openai_chat');
+        $this->title = get_string('localai_chat', 'block_localai_chat');
     }
 
     public function has_config() {
@@ -50,13 +51,13 @@ class block_openai_chat extends block_base {
         }
 
         // Send data to front end
-        $persistconvo = get_config('block_openai_chat', 'persistconvo');
+        $persistconvo = get_config('block_localai_chat', 'persistconvo');
         if (!empty($this->config)) {
-            $persistconvo = (property_exists($this->config, 'persistconvo') && get_config('block_openai_chat', 'allowinstancesettings')) ? $this->config->persistconvo : $persistconvo;
+            $persistconvo = (property_exists($this->config, 'persistconvo') && get_config('block_localai_chat', 'allowinstancesettings')) ? $this->config->persistconvo : $persistconvo;
         }
-        $this->page->requires->js_call_amd('block_openai_chat/lib', 'init', [[
+        $this->page->requires->js_call_amd('block_localai_chat/lib', 'init', [[
             'blockId' => $this->instance->id,
-            'api_type' => get_config('block_openai_chat', 'type') ? get_config('block_openai_chat', 'type') : 'chat',
+            'api_type' => get_config('block_localai_chat', 'type') ? get_config('block_localai_chat', 'type') : 'chat',
             'persistConvo' => $persistconvo
         ]]);
 
@@ -64,18 +65,18 @@ class block_openai_chat extends block_base {
         $showlabelscss = '';
         if (!empty($this->config) && !$this->config->showlabels) {
             $showlabelscss = '
-                .openai_message:before {
+                .localai_message:before {
                     display: none;
                 }
-                .openai_message {
+                .localai_message {
                     margin-bottom: 0.5rem;
                 }
             ';
         }
 
         // First, fetch the global settings for these (and the defaults if not set)
-        $assistantname = get_config('block_openai_chat', 'assistantname') ? get_config('block_openai_chat', 'assistantname') : get_string('defaultassistantname', 'block_openai_chat');
-        $username = get_config('block_openai_chat', 'username') ? get_config('block_openai_chat', 'username') : get_string('defaultusername', 'block_openai_chat');
+        $assistantname = get_config('block_localai_chat', 'assistantname') ? get_config('block_localai_chat', 'assistantname') : get_string('defaultassistantname', 'block_localai_chat');
+        $username = get_config('block_localai_chat', 'username') ? get_config('block_localai_chat', 'username') : get_string('defaultusername', 'block_localai_chat');
 
         // Then, override with local settings if available
         if (!empty($this->config)) {
@@ -94,32 +95,32 @@ class block_openai_chat extends block_base {
 
             <style>
                 ' . $showlabelscss . '
-                .openai_message.user:before {
+                .localai_message.user:before {
                     content: "' . $username . '";
                 }
-                .openai_message.bot:before {
+                .localai_message.bot:before {
                     content: "' . $assistantname . '";
                 }
             </style>
 
-            <div id="openai_chat_log" role="log"></div>
+            <div id="localai_chat_log" role="log"></div>
         ';
 
         if (
-            empty(get_config('block_openai_chat', 'apikey')) && 
-            (!get_config('block_openai_chat', 'allowinstancesettings') || empty($this->config->apikey))
+            empty(get_config('block_localai_chat', 'apikey')) && 
+            (!get_config('block_localai_chat', 'allowinstancesettings') || empty($this->config->apikey))
         ) {
-            $this->content->footer = get_string('apikeymissing', 'block_openai_chat');
+            $this->content->footer = get_string('apikeymissing', 'block_localai_chat');
         } else {
             $contextdata = [
-                'logging_enabled' => get_config('block_openai_chat', 'logging'),
+                'logging_enabled' => get_config('block_localai_chat', 'logging'),
                 'is_edit_mode' => $PAGE->user_is_editing(),
-                'pix_popout' => '/blocks/openai_chat/pix/arrow-up-right-from-square.svg',
-                'pix_arrow_right' => '/blocks/openai_chat/pix/arrow-right.svg',
-                'pix_refresh' => '/blocks/openai_chat/pix/refresh.svg',
+                'pix_popout' => '/blocks/localai_chat/pix/arrow-up-right-from-square.svg',
+                'pix_arrow_right' => '/blocks/localai_chat/pix/arrow-right.svg',
+                'pix_refresh' => '/blocks/localai_chat/pix/refresh.svg',
             ];
 
-            $this->content->footer = $OUTPUT->render_from_template('block_openai_chat/control_bar', $contextdata);
+            $this->content->footer = $OUTPUT->render_from_template('block_localai_chat/control_bar', $contextdata);
         }
 
         return $this->content;
